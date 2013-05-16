eacc0ba7cdfd44c9e84251ef34183e9342771c85
Justin Balthrop <git@justinbalthrop.com>
2013-05-16 15:42:44 -0700

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/teleturn "0.1.0"
+(defproject org.flatland/teleturn "0.1.1-SNAPSHOT"
   :description "Shared code for Telemetry and Turntable's admin interface."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
