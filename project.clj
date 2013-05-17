ed00e5c4ee4100779a8c1b6015e2df715e1dfaed
Justin Balthrop <git@justinbalthrop.com>
2013-05-16 17:26:10 -0700

    0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telegraph "0.1.2"
+(defproject org.flatland/telegraph "0.2.0-SNAPSHOT"
   :description "Make beautiful dashboards for telemetry and turntable."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
