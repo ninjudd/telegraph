90fe01f29cdc11314aea527ff075169799df7ccc
Justin Balthrop <git@justinbalthrop.com>
2013-05-16 17:33:28 -0700

    add resources only classifier

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject org.flatland/telegraph "0.2.0-SNAPSHOT"
   :description "Make beautiful dashboards for telemetry and turntable."
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :classifiers {:resources {:dependencies []
+                            :omit-source true
+                            :compile-path "target/empty"}})
