b88dc398c88e148c05e75cc63f8ee5c045af2e66
Justin Balthrop <git@justinbalthrop.com>
2013-05-17 16:44:21 -0700

    wrap-rewrite so we do not have to specify index.html

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/wakeful "0.5.3"]]
+                 [org.flatland/wakeful "0.5.3"]
+                 [lib-noir "0.5.5"]]
   :plugins [[lein-ring "0.8.5"]]
   :classifiers {:resources {:dependencies []
                             :omit-source true
--- src/flatland/telegraph.clj
+++ src/flatland/telegraph.clj
@@ -1,7 +1,9 @@
 (ns flatland.telegraph
   (:require [flatland.wakeful.core :refer [wakeful]]
-            [flatland.telegraph.graph :as graph]))
+            [flatland.telegraph.graph :as graph]
+            [noir.util.middleware :refer [wrap-rewrites]]))
 
 (def handler
-  (wakeful :root "flatland.telegraph"
-           :config (graph/-init)))
+  (-> (wakeful :root "flatland.telegraph"
+               :config (graph/-init))
+      (wrap-rewrites #"^/telegraph/?$" "/telegraph/index.html")))
