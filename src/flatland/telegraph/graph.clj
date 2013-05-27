bda1e6c0b6113431a10290c19bebff0733a41736
Justin Balthrop <git@justinbalthrop.com>
2013-05-26 21:01:34 -0700

    keywordize keys before saving

--- src/flatland/telegraph/graph.clj
+++ src/flatland/telegraph/graph.clj
@@ -1,22 +1,24 @@
 (ns flatland.telegraph.graph
   (:refer-clojure :exclude [load list])
-  (:require [flatland.utils :refer [file-ref save-versioned!]]))
+  (:require [flatland.utils :refer [file-ref save-versioned!]]
+            [clojure.walk :refer [keywordize-keys]]))
 
 (defn -init [file]
   {:graphs (file-ref file)})
 
-(defn save! [{:keys [graphs]} {{:strs [name version] :as opts} :body}]
-  (if (empty? name) 
-    {:status 400
-     :body {:error "name is required"}}
-    (if-let [version (save-versioned! graphs name version opts)]
-      {:body {:version version}}
+(defn save! [{:keys [graphs]} request]
+  (let [{:keys [name version] :as opts} (keywordize-keys (:body request))]
+    (if (empty? name)
       {:status 400
-       :body {:error (format "version %s of %s is stale" version name)}})))
+       :body {:error "name is required"}}
+      (if-let [version (save-versioned! graphs name version opts)]
+        {:body {:name name, :version version}}
+        {:status 400
+         :body {:error (format "version %s of %s is stale" version name)}}))))
 
-(defn load [{:keys [graphs]} {{:strs [name]} :params :as opts}]
-(prn opts)
-  {:body (get @graphs name)})
+(defn load [{:keys [graphs]} request]
+  (let [name (get (:params request) "name")]
+    {:body (get @graphs name)}))
 
 (defn list [{:keys [graphs]} _]
   {:body (keys @graphs)})
