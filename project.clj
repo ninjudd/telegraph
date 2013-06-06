829dea006e387add89e111d8a937a7d08d53ea3f
Justin Balthrop <git@justinbalthrop.com>
2013-06-06 12:54:13 -0700

    0.2.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telegraph "0.2.10"
+(defproject org.flatland/telegraph "0.2.11"
   :description "Make beautiful dashboards for telemetry and turntable."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
