e8b1840024bb7b64f47b7dd494bb6a0071679243
Justin Balthrop <git@justinbalthrop.com>
2013-06-20 15:29:30 -0700

    add wakeful back for now

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.flatland/wakeful "0.5.3"]
                  [org.flatland/telegraph-ui "0.4.0-SNAPSHOT"]
                  [lib-noir "0.5.5"]]
   :plugins [[lein-ring "0.8.5"]]
