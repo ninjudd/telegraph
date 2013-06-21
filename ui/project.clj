579ef316bd81daafad1b3362089342dceec0f9bb
Justin Balthrop <git@justinbalthrop.com>
2013-06-21 15:27:12 -0700

    0.4.0-beta1

--- ui/project.clj
+++ ui/project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telegraph-ui "0.4.0-SNAPSHOT"
+(defproject org.flatland/telegraph-ui "0.4.0-beta1"
   :description "Telegraph admin tool."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
