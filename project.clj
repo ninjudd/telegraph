beb447233e8ef5c4f633e24de6e3a7229361524d
Justin Balthrop <git@justinbalthrop.com>
2013-06-21 12:11:23 -0700

    resting 0.1.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.flatland/resting "0.1.1"]
+                 [org.flatland/resting "0.1.3"]
                  [org.flatland/telegraph-ui "0.4.0-SNAPSHOT"]
                  [lib-noir "0.5.5"]]
   :plugins [[lein-ring "0.8.5"]]
--- ui/project.clj
+++ ui/project.clj
@@ -3,4 +3,4 @@
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.flatland/resting-js "0.1.2"]])
+  :dependencies [[org.flatland/resting-js "0.1.3"]])
