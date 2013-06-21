e5a1e8c2761881837fe413cbc161db7153924d86
Justin Balthrop <git@justinbalthrop.com>
2013-06-21 11:10:16 -0700

    move resting.js out into org.flatland/resting-js

--- ui/project.clj
+++ ui/project.clj
@@ -2,4 +2,5 @@
   :description "Telegraph admin tool."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.flatland/resting-js "0.1.1"]])
