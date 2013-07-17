8eb9087561e9921b21c5574702fe479b5664ede1
Justin Balthrop <git@justinbalthrop.com>
2013-07-17 13:49:27 -0700

    use raw github file until cdnjs is updated

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.flatland/telegraph "0.4.1"
+(defproject org.flatland/telegraph "0.4.2"
   :description "Make beautiful dashboards for telemetry and turntable."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.flatland/resting "0.1.4"]
-                 [org.flatland/telegraph-ui "0.4.1"]
+                 [org.flatland/telegraph-ui "0.4.2"]
                  [lib-noir "0.5.5"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler flatland.telegraph/handler
--- ui/project.clj
+++ ui/project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telegraph-ui "0.4.1"
+(defproject org.flatland/telegraph-ui "0.4.2"
   :description "Telegraph web interface."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
