6f395228653aa70189411154172eb574b8ff24f2
Justin Balthrop <git@justinbalthrop.com>
2013-08-08 11:53:54 -0700

    update resting-js to 0.1.8

--- js/project.clj
+++ js/project.clj
@@ -1,6 +1,6 @@
-(defproject org.flatland/telegraph-js "0.4.4"
+(defproject org.flatland/telegraph-js "0.4.5"
   :description "Telegraph javascript libraries."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.flatland/resting-js "0.1.7"]])
+  :dependencies [[org.flatland/resting-js "0.1.8"]])
