1f21eea7a81932e35b3057cabdf279bd1d543a8c
Justin Balthrop <git@justinbalthrop.com>
2013-08-27 15:28:41 -0700

    update telegraph-js and telegraph-ui

--- js/project.clj
+++ js/project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telegraph-js "0.4.6"
+(defproject org.flatland/telegraph-js "0.4.7"
   :description "Telegraph javascript libraries."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
