b5be95d6c398efdf5777de26d00de316667ec1c4
Justin Balthrop <git@justinbalthrop.com>
2013-08-27 19:15:30 -0700

    update versions

--- ui/project.clj
+++ ui/project.clj
@@ -1,6 +1,6 @@
-(defproject org.flatland/telegraph-ui "0.4.8"
+(defproject org.flatland/telegraph-ui "0.4.9"
   :description "Telegraph web interface."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.flatland/telegraph-js "0.4.7"]])
+  :dependencies [[org.flatland/telegraph-js "0.4.8"]])
