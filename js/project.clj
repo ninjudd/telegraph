1674ee592a7bedf05ac7bb8b37f90c2c2708c116
Justin Balthrop <git@justinbalthrop.com>
2013-09-04 10:15:18 -0700

    update versions

--- js/project.clj
+++ js/project.clj
@@ -1,6 +1,6 @@
-(defproject org.flatland/telegraph-js "0.4.9"
+(defproject org.flatland/telegraph-js "0.4.10"
   :description "Telegraph javascript libraries."
   :url "http://github.com/flatland/telegraph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.flatland/resting-js "0.1.8"]])
+  :dependencies [[org.flatland/resting-js "0.1.9"]])
