fb5addd1edf5934b36bd198b3766cb1afca6a653
Justin Balthrop <git@justinbalthrop.com>
2013-10-18 16:00:28 -0700

    update nvd3 to 1.1.13-beta

--- ui/resources/public/common.js
+++ ui/resources/public/common.js
@@ -7,8 +7,8 @@ requirejs.config({
     underscore:         "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore",
     underscore_contrib: "//cdnjs.cloudflare.com/ajax/libs/underscore-contrib/0.1.4/underscore-contrib",
     underscore_string:  "//raw.github.com/epeli/underscore.string/2.3.3/dist/underscore.string.min",
-    d3:                 "//cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2",
-    nvd3:               "//cdnjs.cloudflare.com/ajax/libs/nvd3/0.9/nv.d3",
+    d3:                 "//cdnjs.cloudflare.com/ajax/libs/d3/3.3.3/d3",
+    nvd3:               "//cdnjs.cloudflare.com/ajax/libs/nvd3/1.1.13-beta/nv.d3",
     backbone:           "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone",
     chosen:             "//cdnjs.cloudflare.com/ajax/libs/chosen/0.9.15/chosen.jquery.min"
   },
